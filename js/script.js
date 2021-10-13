ea0b8b7700fad46fa55d8b09fe42d0185fcd0130
Matthew Carson <matthew.carson@pm.me>
2021-10-13 17:10:57 -0400

    General clean up

--- js/script.js
+++ js/script.js
@@ -3,9 +3,46 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+const quotes = [
+  {
+    quote: 'Expecting the world to treat you fairly because you are a good person is a little like expecting the bull not to attack you because you are a vegetarian.',
+    source: 'Dennis Wholey'
+  },
+  {
+    quote: "Before you judge a man, walk a mile in his shoes. After that who cares?... He's a mile away and you've got his shoes",
+    source: 'Billy Connolly'
+  },
+  {
+    quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
+    source: 'Franklin D. Roosevelt'
+  },
+  {
+    quote: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
+    source: 'Benjamin Franklin'
+  },
+  {
+    quote: 'If you think you are too small to make a difference, try sleeping with a mosquito',
+    source:  'Dalai Lama'
+  },
+  {
+    quote: 'May the Force be with you.',
+    source: 'Obi-Wan Kenobi',
+    citation: 'Star Wars',
+    year: '1977'
+  },
+  {
+    quote: "There's no crying in baseball!",
+    source: 'Jimmy Dugan',
+    citation: 'A League of their Own',
+    year: '1992'
+  },
+  {
+    quote: 'Nobody puts Baby in a corner.',
+    source: 'Johnny Castle',
+    citation: 'Johnny Castle',
+    year: '1987'
+  },
+]
 
 const getRandomColor = () => {
   const colorElements = '0123456789ABCDEF'.split('')
@@ -21,48 +58,7 @@ const getRandomColor = () => {
   document.querySelector('button').setAttribute('style', `background-color: ${colorString}`)
 }
 
-/*** 
- * `quotes` array 
-***/
-const quotes = [
-  {
-    quote: "Expecting the world to treat you fairly because you are a good person is a little like expecting the bull not to attack you because you are a vegetarian.",
-    source: "Dennis Wholey",
-    citation: "https://www.azquotes.com/quote/540836",
-  },
-  {
-    quote: "Before you judge a man, walk a mile in his shoes. After that who cares?... He's a mile away and you've got his shoes",
-    source: 'Billy Connolly',
-  },
-  {
-    quote: "When you reach the end of your rope, tie a knot in it and hang on.",
-    source: "Franklin D. Roosevelt"
-  },
-  {
-    quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-    source: "Benjamin Franklin"
-  },
-  {
-    quote: "If you think you are too small to make a difference, try sleeping with a mosquito",
-    source:  "Dalai Lama",
-  },
-  {
-    quote: "May the Force be with you.",
-    source: "Star Wars",
-    year: '1977',
-  },
-  {
-    quote: "There's no crying in baseball!",
-    source: "A League of their Own",
-    year: '1992',
-  },
-  {
-    quote: "Nobody puts Baby in a corner.",
-    source: "Dirty Dancing",
-    year: '1987',
-  },
-]
-
+// Updates the 'New Quote Countdown'.
 const updateCountdown = () => {
   if (timer === 1) {
     printQuote()
@@ -73,6 +69,7 @@ const updateCountdown = () => {
   updateCountdownDisplay()
 }
 
+// Reset the countdown
 const resetCountdown = () => {
   timer = 5
   clearInterval(countdown)
@@ -80,62 +77,50 @@ const resetCountdown = () => {
   countdown = setInterval(updateCountdown, 1000)
 }
 
+// Sets the countdown display on the page.
 const updateCountdownDisplay = () => {
   document.querySelector('.time').innerHTML = `${timer} ${timer > 1 ? 'seconds' : 'second' }`
 }
 
-let timer = 5
-
-
-/***
- * `getRandomQuote` function
-***/
+/* 
+ * Get random number and return the quote at that index in the quotes array.
+ * If the random number is the same as that of the currently displayed 
+ * quote's index, generate a new number (prevents the same quote 
+ * from being displayed again).
+ */
 const getRandomQuote = () => {
-  let newQuoteIndex = Math.floor(Math.random() * quotes.length)
-
-  /* Get random number and return the quote at that index in the quotes array.
-   * If the random number is the same as that of the currently displayed 
-   * quote's index, generate a new number (prevents the same quote 
-   * from being displayed again).
-   */
+  let newQuoteIndex
+  
   do {
     newQuoteIndex = Math.floor(Math.random() * quotes.length)
   } while (newQuoteIndex === currentQuoteIndex)
-
+  
   currentQuoteIndex = newQuoteIndex
   return quotes[newQuoteIndex]
 }
 
-
-
-let countdown = setInterval(updateCountdown, 1000);
-
-/***
- * `printQuote` function
-***/
+/*
+ * Print the quote to the page. Includes resetting new quote countdown and
+ * and getting a new color for the page background. 
+ */
 const printQuote = () => {
   resetCountdown()
   getRandomColor()
-
-  let quote = getRandomQuote()
   
+  const quote = getRandomQuote()
+
+  // Build source string. If a quote has a year and/or citation attribute include them.
+  const source = `${quote.source}${quote.year ? `<span class="year">${quote.year}</span>` : ''}${quote.citation ? `<span class="citation">${quote.citation}</span>` : ''}`
+
   document.querySelector('.quote').innerHTML = quote.quote
-  document.querySelector('.source').innerHTML = `
-    ${quote.source}${quote.year ? `<span class="year">${quote.year}</span>` : ''}
-  `
+  document.querySelector('.source').innerHTML = source
 }
 
-
-
+let timer = 5
 let currentQuoteIndex = 0
+let countdown = setInterval(updateCountdown, 1000);
+
 getRandomColor()
 printQuote()
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
