f98a3f8b0fd738f87793cae428f7c89742b3c7a7
Matthew Carson <matthew.carson@pm.me>
2021-10-13 19:18:42 -0400

    Add category attribute to quotes/General clean-up and comments.

--- js/script.js
+++ js/script.js
@@ -6,11 +6,13 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
     quote: 'Expecting the world to treat you fairly because you are a good person is a little like expecting the bull not to attack you because you are a vegetarian.',
-    source: 'Dennis Wholey'
+    source: 'Dennis Wholey',
+    category: 'funny'
   },
   {
     quote: "Before you judge a man, walk a mile in his shoes. After that who cares?... He's a mile away and you've got his shoes",
-    source: 'Billy Connolly'
+    source: 'Billy Connolly',
+    category: 'funny'
   },
   {
     quote: 'When you reach the end of your rope, tie a knot in it and hang on.',
@@ -22,25 +24,29 @@ const quotes = [
   },
   {
     quote: 'If you think you are too small to make a difference, try sleeping with a mosquito',
-    source:  'Dalai Lama'
+    source:  'Dalai Lama',
+    category: 'funny'
   },
   {
     quote: 'May the Force be with you.',
     source: 'Obi-Wan Kenobi',
     citation: 'Star Wars',
-    year: '1977'
+    year: '1977',
+    category: 'film'
   },
   {
     quote: "There's no crying in baseball!",
     source: 'Jimmy Dugan',
     citation: 'A League of their Own',
-    year: '1992'
+    year: '1992',
+    category: 'film'
   },
   {
     quote: 'Nobody puts Baby in a corner.',
     source: 'Johnny Castle',
     citation: 'Johnny Castle',
-    year: '1987'
+    year: '1987',
+    category: 'film'
   },
 ]
 
@@ -53,9 +59,12 @@ const getRandomColor = () => {
     colorString += colorElements[Math.floor(Math.random() * colorElements.length)]
   }
 
+  // Set the style of the effected elements to the random color.
   document.querySelector('.container').setAttribute('style', `color: ${colorString}`)
   document.querySelector('body').setAttribute('style', `background-color: ${colorString}`)
   document.querySelector('button').setAttribute('style', `background-color: ${colorString}`)
+  document.querySelector('#category-film').setAttribute('style', `background-color: ${colorString}`)
+  document.querySelector('#category-funny').setAttribute('style', `background-color: ${colorString}`)
 }
 
 // Updates the 'New Quote Countdown'.
@@ -69,12 +78,12 @@ const updateCountdown = () => {
   updateCountdownDisplay()
 }
 
-// Reset the countdown
+// Resets the countdown
 const resetCountdown = () => {
   timer = 5
-  clearInterval(countdown)
+  clearInterval(countdown) // Clears the countdown/interval.
   updateCountdownDisplay()
-  countdown = setInterval(updateCountdown, 1000)
+  countdown = setInterval(updateCountdown, 1000) // Starts a new countdown/interval.
 }
 
 // Sets the countdown display on the page.
@@ -82,34 +91,52 @@ const updateCountdownDisplay = () => {
   document.querySelector('.time').innerHTML = `${timer} ${timer > 1 ? 'seconds' : 'second' }`
 }
 
+const getFilmQuote = () => printQuote('film')
+
+const getFunnyQuote = () => printQuote('funny')
+
 /* 
  * Get random number and return the quote at that index in the quotes array.
  * If the random number is the same as that of the currently displayed 
  * quote's index, generate a new number (prevents the same quote 
  * from being displayed again).
  */
-const getRandomQuote = () => {
+const getRandomQuote = category => {
   let newQuoteIndex
-  
+  let filteredQuotes
+  console.log('category =', category)
+  if (category) {
+    filteredQuotes = quotes.filter(quote => {
+      if (quote.category && quote.category === category) return quote
+    })
+  } else {
+    filteredQuotes = quotes
+  }
+
   do {
-    newQuoteIndex = Math.floor(Math.random() * quotes.length)
+    newQuoteIndex = Math.floor(Math.random() * filteredQuotes.length)
   } while (newQuoteIndex === currentQuoteIndex)
   
   currentQuoteIndex = newQuoteIndex
-  return quotes[newQuoteIndex]
+  return filteredQuotes[newQuoteIndex]
 }
 
 /*
  * Print the quote to the page. Includes resetting new quote countdown and
  * and getting a new color for the page background. 
  */
-const printQuote = () => {
+const printQuote = (category = null) => {
   resetCountdown()
   getRandomColor()
-  
-  const quote = getRandomQuote()
 
-  // Build source string. If a quote has a year and/or citation attribute include them.
+  // By default, functions called from an event listener pass
+  // the event as an argument. If the argument passed is
+  // not a string (e.g. it's an event) make sure it 
+  // is not passed to the getRandomQuote function.
+  if (typeof category !== 'string') category = null
+  const quote = getRandomQuote(category)
+
+  // Build source string. If a quote has a year and/or citation attributes include them.
   const source = `${quote.source}${quote.year ? `<span class="year">${quote.year}</span>` : ''}${quote.citation ? `<span class="citation">${quote.citation}</span>` : ''}`
 
   document.querySelector('.quote').innerHTML = quote.quote
@@ -124,3 +151,5 @@ getRandomColor()
 printQuote()
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('category-funny').addEventListener("click", getFunnyQuote, false);
+document.getElementById('category-film').addEventListener("click", getFilmQuote, false);
